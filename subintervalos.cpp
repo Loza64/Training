33bbb500e6487c4330a177a37370ef2e7108c38e
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-08-29 11:48:46 -0600

    Update subintervalos.cpp

--- subintervalos.cpp
+++ subintervalos.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  
+using namespace std;  
 
-int main(){
+int main() {  
+    int array[] = {1, 2, 3, -2, 5, -1, 2, -3, 4, 6, -1, 2};  
+    int n = sizeof(array) / sizeof(array[0]);  //Arrays lengh
+    
+    int max_sum = array[0]; 
+    int current_sum = array[0];
 
-    int[] array = {1,3,5,7,3,2,3,5,7,2,3,6,8,3};
-    cout << array[0];
+    for (int i = 1; i < n; ++i) {  
+        current_sum = max(array[i], current_sum + array[i]); 
+        max_sum = max(max_sum, current_sum);
+    }  
 
+    cout << "El subintervalo maximo es: " << max_sum << endl;  
+    return 0;  
 }
