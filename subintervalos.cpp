4e49beb7c4ebdb82bca0abb58f98e643276ef9cf
Roberto Loza <loza@ASUS-X515EA.ufostartdevelopment.com>
2024-08-30 10:55:40 -0600

    subintervalos

--- subintervalos.cpp
+++ subintervalos.cpp
@@ -1,18 +1,39 @@
 #include <iostream>  
+#include <vector>  
 using namespace std;  
 
-int main() {  
-    int array[] = {1, 2, 3, -2, 5, -1, 2, -3, 4, 6, -1, 2};  
-    int n = sizeof(array) / sizeof(array[0]);  //Arrays lengh
-    
-    int max_sum = array[0]; 
-    int current_sum = array[0];
+pair<int, pair<int, int>> encontrarSubintervaloMayor(const vector<int>& arr) {  
+    int max_sum = arr[0];
+    int current_sum = arr[0]; 
+    int start = 0; 
+    int end = 0;  
+    int temp_start = 0; 
 
-    for (int i = 1; i < n; ++i) {  
-        current_sum = max(array[i], current_sum + array[i]); 
-        max_sum = max(max_sum, current_sum);
+    for (int i = 1; i < arr.size(); i++) {  
+        current_sum += arr[i];  
+
+
+        if (current_sum > max_sum) {  
+            max_sum = current_sum;  
+            start = temp_start;  
+            end = i;  
+        }  
+
+        if (current_sum < 0) {  
+            current_sum = 0;  
+            temp_start = i + 1; 
+        }  
     }  
 
-    cout << "El subintervalo maximo es: " << max_sum << endl;  
+    return {max_sum, {start, end}}; 
+}  
+
+int main() {  
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};  
+    auto resultado = encontrarSubintervaloMayor(arr);  
+    
+    cout << "La suma máxima es: " << resultado.first << endl;  
+    cout << "El subintervalo mayor está entre los índices: " << resultado.second.first << " y " << resultado.second.second << endl;  
+
     return 0;  
 }
